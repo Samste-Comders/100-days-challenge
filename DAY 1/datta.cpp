167d4d113f6f79208d0b3c324d381b15ad8ccf3c
Datta Gangji <103048010+the-att-21@users.noreply.github.com>
2022-10-16 14:04:09 +0530

    2nd question solution updated

--- DAY 1/datta.cpp
+++ DAY 1/datta.cpp
@@ -1,3 +1,4 @@
+//TWO SUM SOLUTION
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -15,5 +16,50 @@ public:
             if(flag) break;
         }
         return ans;
+    }
+};
+
+// Remove nth node from end of LL Solution
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int length(ListNode *&head)
+    {
+        ListNode *t = head;
+        int l = 0;
+
+        while (t != NULL)
+        {
+            l++;
+            t = t->next;
+        }
+        return l;
+    }
+    void delete1node(ListNode*& head){
+        head=head->next;
+    }
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if(head->next==NULL) return NULL;
+        ListNode* temp=head;
+        int l=length(head);
+        int loop=l-n-1;
+        if(loop<0){
+            delete1node(head);
+            return head;
+        }
+        while(loop--){
+            temp=temp->next;
+        }
+        temp->next=temp->next->next;
+        return head;
     }
 };
