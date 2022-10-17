439a15427ba6b1bac3afe09f31b132b0a216e330
HimanshuMude <98302342+HimanshuMude@users.noreply.github.com>
2022-10-17 19:26:44 +0530

    Added solution to question 2

--- Day 2/himanshu.cpp
+++ Day 2/himanshu.cpp
@@ -1,3 +1,4 @@
+//Question 1 solution
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
@@ -6,5 +7,34 @@ public:
         //then check if all 26 alphabets occur once or not .
         if(s.size()==26) return true;
         return false;
+    }
+};
+
+//Question 2 solution
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int carry=0;
+        int n=digits.size();
+        digits[n-1]+=1;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(digits[i]+carry>9)
+            {
+                digits[i]=0;
+                carry=1;
+            }
+            else
+            {
+                digits[i]+=carry;
+                carry=0;
+            }
+        }
+        if(carry!=0)
+        {
+            digits.insert(digits.begin(),1);
+        }
+        return digits;
     }
 };
