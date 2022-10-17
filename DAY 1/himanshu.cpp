d2668269b580c98fe064f36c3c96a9616122a3f2
HimanshuMude <98302342+HimanshuMude@users.noreply.github.com>
2022-10-17 19:15:03 +0530

    Added solution for 1st and 2nd question

--- DAY 1/himanshu.cpp
+++ DAY 1/himanshu.cpp
@@ -1,3 +1,47 @@
+//Question 1 solution
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int>m;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(m.find(target-nums[i])!=m.end())
+            {
+                return {i,m[target-nums[i]]};
+            }
+            else
+            {
+                m[nums[i]]=i;
+            }
+        }
+        return {};
+    }
+};
+
+
+//Question 2 solution
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* fast=head,*slow=head;
+        while(n--) fast=fast->next;
+        if(fast==NULL)
+        {
+            return head->next;
+        }
+        while(fast->next!=NULL)
+        {
+            fast=fast->next;
+            slow=slow->next;
+        }
+        slow->next=slow->next->next;
+        return head;
+    }
+};
+
+
+//Question 3 solution
 class Solution {
 public:
     bool isValid(string st) {
