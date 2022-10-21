643fe631a25cdfbd0b63f672a7f5434cbbc01e53
ThunderSmoker <103036441+ThunderSmoker@users.noreply.github.com>
2022-10-21 19:01:30 +0530

    Create Aradhya.cpp

---
+++ Day 2/Aradhya.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        unordered_map<char,int> mp;
+        for(int i=0;i<sentence.size();i++){
+            mp[sentence[i]]++;
+        }
+        for(char i='a';i<='z';i++){
+            if(mp[i]==0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
