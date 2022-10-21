9f829c0160932f02a071699d7eb2075648a629b6
ThunderSmoker <103036441+ThunderSmoker@users.noreply.github.com>
2022-10-21 19:03:24 +0530

    Update Aradhya.cpp

--- Day 2/Aradhya.cpp
+++ Day 2/Aradhya.cpp
@@ -1,3 +1,5 @@
+// himanshu que solution
+
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
@@ -11,5 +13,28 @@ public:
             }
         }
         return true;
+    }
+};
+
+//datta que solution
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int carry = 1;
+        
+        for(int i = digits.size() - 1; i>=0; i--){
+            int sum = digits[i] + carry;
+            
+            digits[i] = sum%10;
+            
+            carry = sum/10;
+            
+            if(carry == 0) break;
+        }
+        
+        if(carry == 1) digits.insert(digits.begin(), 1);
+        
+        return digits;
     }
 };
